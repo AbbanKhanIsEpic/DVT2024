c2f2a8fc14e428a725be3ef590a3eeb86011b3c7
AbbanKhanIsEpic <abbanisepic@gmail.com>
2024-02-20 14:05:00 +0200

    screen

--- app/contact.js
+++ app/contact.js
@@ -1,5 +1,5 @@
-const togglePasswordView = document.querySelector("#viewPasswordInput");
-const passwordInput = document.querySelector("#passwordInput");
+const toggleEmailView = document.querySelector("#viewEmailInput");
+const emailInput = document.querySelector("#emailInput");
 
 const toggleNameView = document.querySelector("#viewNameInput");
 const nameInput = document.querySelector("#nameInput");
@@ -9,10 +9,13 @@ const phoneInput = document.querySelector("#phoneInput");
 
 const loginButton = document.querySelector("#continueButton");
 
-togglePasswordView.addEventListener("click", function () {
-  let nextView = togglePasswordView.innerHTML == "Open" ? "Close" : "Open";
-  togglePasswordView.innerHTML = nextView;
-  passwordInput.type = nextView == "Open" ? "text" : "password";
+const registerError = document.getElementById("registerError");
+const errorMessage = document.getElementById("errorMessage");
+
+toggleEmailView.addEventListener("click", function () {
+  let nextView = toggleEmailView.innerHTML == "Open" ? "Close" : "Open";
+  toggleEmailView.innerHTML = nextView;
+  emailInput.type = nextView == "Open" ? "text" : "password";
 });
 
 toggleNameView.addEventListener("click", function () {
@@ -29,26 +32,23 @@ togglePhoneView.addEventListener("click", function () {
 
 loginButton.addEventListener("click", function () {
   nameInput.classList.replace("border-danger", "border-black");
-  passwordInput.classList.replace("border-danger", "border-black");
+  emailInput.classList.replace("border-danger", "border-black");
   phoneInput.classList.replace("border-danger", "border-black");
 
-  const registerError = document.getElementById("registerError");
-  const errorMessage = document.getElementById("errorMessage");
+  registerError.style.visibility = "hidden";
 
-  registerError.style.visibility = "invisible";
-
-  let name = nameInput.value;
-  let password = passwordInput.value;
-  let phoneNumber = phoneInput.value;
+  const name = nameInput.value;
+  const email = emailInput.value;
+  const phoneNumber = phoneInput.value;
 
   //Presence check
   if (name.length == 0) {
     nameInput.classList.replace("border-black", "border-danger");
     errorMessage.innerText = "You have to put a name";
     registerError.style.visibility = "visible";
-  } else if (password.length == 0) {
-    passwordInput.classList.replace("border-black", "border-danger");
-    errorMessage.innerText = "You have to enter your password";
+  } else if (email.length == 0) {
+    emailInput.classList.replace("border-black", "border-danger");
+    errorMessage.innerText = "You have to enter your email";
     registerError.style.visibility = "visible";
   } else if (phoneNumber.length == 0) {
     phoneInput.classList.replace("border-black", "border-danger");
@@ -61,27 +61,32 @@ loginButton.addEventListener("click", function () {
     nameInput.classList.replace("border-black", "border-danger");
     errorMessage.innerText = "Your name is too long";
     registerError.style.visibility = "visible";
-  } else if (!name.test("/^[a-zA-Z]+$/")) {
+  } else if (!(name.match(/^[a-zA-Z]+$/))) {
     nameInput.classList.replace("border-black", "border-danger");
     errorMessage.innerText = "Your name can only have letters";
     registerError.style.visibility = "visible";
   }
 
   //Checking stuff for password
-  else if (password.length < 8) {
-    passwordInput.classList.replace("border-black", "border-danger");
-    errorMessage.innerText = "Your password is too short";
+  else if (email.length < 6) {
+    emailInput.classList.replace("border-black", "border-danger");
+    errorMessage.innerText = "Your email is too short";
     registerError.style.visibility = "visible";
-  } else if (password.length > 100) {
-    passwordInput.classList.replace("border-black", "border-danger");
-    errorMessage.innerText = "Your password is too long";
+  } else if (email.length > 254) {
+    emailInput.classList.replace("border-black", "border-danger");
+    errorMessage.innerText = "Your email is too long";
+    registerError.style.visibility = "visible";
+  }
+  else if(!email.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)){
+    emailInput.classList.replace("border-black", "border-danger");
+    errorMessage.innerText = "Your email is incorrect";
     registerError.style.visibility = "visible";
   }
 
   //Checking stuff for phone number
   else if (
     phoneNumber.length != 10 ||
-    !phoneInput.test("^(+27|0)[6-8][0-9]{8}$")
+    !phoneNumber.match(/^(\+27|0)[6-8][0-9]{8}$/)
   ) {
     phoneInput.classList.replace("border-black", "border-danger");
     errorMessage.innerText = "Your phone number is not valid";
